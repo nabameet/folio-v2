9f400cccd8a68ad2b3da830753088e6542fb8c56
nabameet <thenabameet@gmail.com>
2025-08-27 20:59:41 +0530

    added blend mode to /play

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,7 +1,7 @@
 export default function Play() {
   return (
     <>
-      <div className="px-8 pt-40 md:px-12">
+      <div className="px-8 pt-40 md:px-12 text-background mix-blend-difference dark:text-foreground">
         <p className="">coming soon: &nbsp;&nbsp;<span className="italic">under construction</span></p>
       </div>
     </>
