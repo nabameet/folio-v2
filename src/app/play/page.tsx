28acc44b8c535f57eb3770f3bdc93c2c8cf880ec
nabameet <thenabameet@gmail.com>
2025-08-30 00:40:39 +0530

    added logo to /play

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,9 +1,15 @@
+import Logo from "@/components/logo/Logo";
+
 export default function Play() {
   return (
     <>
-      <div className="px-8 pt-40 md:px-12 text-background mix-blend-difference dark:text-foreground">
-        <p className="">coming soon: &nbsp;&nbsp;<span className="italic">under construction</span></p>
-      </div>
+    <main className="flex relative justify-center stroke-background dark:stroke-foreground text-background mix-blend-difference dark:text-foreground items-center w-full h-screen">
+      <Logo className="w-full h-full" />
+      <p className="absolute top-0 left-0 px-8 pt-40 md:px-12 flex flex-col gap-4">
+        coming soon:
+        <span className="italic">under construction</span>
+      </p>
+    </main>
     </>
   );
 }
