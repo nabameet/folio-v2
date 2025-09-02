be0c8ec29dcff6638db6b14da8561e30c2a35006
nabameet <thenabameet@gmail.com>
2025-09-02 17:09:29 +0530

    added better project hover

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,19 @@
-"use client"
+"use client";
 
 import Logo from "@/components/logo/Logo";
 import { ProjectList } from "@/cms/organisms/ProjectList";
-import Image from "next/image";
 import Footer from "@/components/Footer";
 import { useState } from "react";
 import { MDXContent } from "@/cms/types";
+import ProjectSnapshot from "./ProjectSnapshot";
 
-export default function Home({projects} : {projects: MDXContent[]}) {
-	// State to track the currently hovered project's image
-  const [currentImage, setCurrentImage] = useState<{ src: string; alt: string } | null>(null);
+export default function Home({ projects }: { projects: MDXContent[] }) {
+  // State to track the currently hovered project's image
+  const [currentImage, setCurrentImage] = useState<{
+    src: string;
+    alt: string;
+    tags: string;
+  } | null>(null);
 
   // Show image when there's a hovered project
   const showImage = currentImage !== null;
@@ -18,7 +22,8 @@ export default function Home({projects} : {projects: MDXContent[]}) {
   const handleProjectHover = (project: MDXContent) => {
     setCurrentImage({
       src: project.metadata.image,
-      alt: project.metadata.title
+      alt: project.metadata.title,
+      tags: project.metadata.tags,
     });
   };
 
@@ -27,19 +32,23 @@ export default function Home({projects} : {projects: MDXContent[]}) {
     setCurrentImage(null);
   };
 
-	return (
+  return (
     <>
-		<main className="flex relative justify-center items-center w-full h-screen"> {/* stroke-background dark:stroke-foreground text-background mix-blend-difference dark:text-foreground */}
+      <main className="flex relative justify-center items-center w-full h-screen">
+        {" "}
+        {/* stroke-background dark:stroke-foreground text-background mix-blend-difference dark:text-foreground */}
         <Logo className="w-full h-full" />
-        { currentImage && <Image className={`w-1/2 -z-50 fixed right-0 px-8 md:px-12 object-cover ${showImage? "visible": "invisible"}`} src={currentImage?.src} alt={currentImage?.alt} width={1400} height={1080} /> }
+        {currentImage && (
+          <ProjectSnapshot showImage={showImage} currentImage={currentImage} />
+        )}
         <ProjectList
           className="absolute top-0 left-0 px-8 pt-40 md:px-12"
           projects={projects}
-					onProjectHover={handleProjectHover}
+          onProjectHover={handleProjectHover}
           onProjectHoverEnd={handleProjectHoverEnd}
         />
         <Footer />
-    </main>
+      </main>
     </>
-  )
+  );
 }
---
+++ src/components/ProjectSnapshot.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function ProjectSnapshot({
+  showImage,
+  currentImage,
+}: {
+  showImage: boolean;
+  currentImage: { src: string; alt: string; tags: string };
+}) {
+  return (
+    <div className="flex flex-col items-end fixed right-0 py-18 px-8 md:py-36 md:px-12 h-full w-auto z-50">
+      <Image
+        className={`object-cover h-full w-full ${
+          showImage ? "visible" : "invisible"
+        }`}
+        src={currentImage.src}
+        alt={currentImage.alt}
+        width={1400}
+        height={1080}
+      />
+      <div>
+        {currentImage.tags.split(",").map((tag, key) => (
+          <span
+            key={key}
+            className="pl-12 lg:pl-20 lg:pr-8 pr-4 text-xl lg:text-3xl border border-1.5 border-foreground rounded-[100%]"
+          >
+            {tag.trim()}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
