9d1962dcce2c648fd7ccfab7faae115769fb1883
nabameet <thenabameet@gmail.com>
2025-09-03 00:30:35 +0530

    added mail icon

--- src/components/LayoutClient.tsx
+++ src/components/LayoutClient.tsx
@@ -46,16 +46,28 @@ export const LayoutClient = ({ children }: { children: React.ReactNode }) => {
             <p>
               {contactPrechorusText}
               <br />
-              {contactTitleText}
-              send a &nbsp;
-              <Link
-                target="_blank"
-                rel="noopener noreferrer"
-                className="italic underline"
-                href={"mailto:" + email}
-              >
-                {email}
-              </Link>
+              <span className="flex items-center">
+                {contactTitleText}
+                send a &nbsp;
+                <Link
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="italic underline"
+                  href={"mailto:" + email}
+                >
+                  {email}
+                </Link>
+                &nbsp;&nbsp;
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-4"
+                >
+                  <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                  <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                </svg>
+              </span>
             </p>
           </InfoAbout>
           <InfoContact xUsername={xUsername}>
