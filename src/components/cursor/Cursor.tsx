fd43ea8463c02d536e0847e7b11268a7cb34ecb7
nabameet <thenabameet@gmail.com>
2025-09-17 18:45:14 +0530

    hide cursor on touch devices

--- src/components/cursor/Cursor.tsx
+++ src/components/cursor/Cursor.tsx
@@ -6,9 +6,9 @@ export const Cursor = () => {
   useCursorEffect();
 
   return (
-    <>
+    <div className="hide-on-touch">
       <div className="cursorFollower pointer-events-none fixed z-[9999] size-20 rounded-full bg-[radial-gradient(circle_at_50%_50%,rgba(11,14,19,1),rgba(255,255,255,0)),url('/noise.svg')] mix-blend-difference [filter:contrast(130%)_brightness(1000%)_invert(100%)]" />
       <div className="cursorCustom bg-background dark:bg-foreground pointer-events-none fixed z-[9999] size-1 rounded-full mix-blend-difference" />
-    </>
+    </div>
   );
 };
